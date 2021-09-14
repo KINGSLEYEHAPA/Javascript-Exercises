cb270194ddd08cd1e5250b9fc5053b6eddc5769e
KINGSLEYEHAPA <kessity09@gmail.com>
2021-09-14 15:30:12 +0100

    Adding files for Array Basics

---
+++ Array exercises/array-basics-exercises.js
@@ -0,0 +1,18 @@
+// Array Basics Exercises
+// 1. favoriteFoods= [ "Beans", "Fried Plantain","Yam"];
+// 2. favoriteFoods.[1];
+// 3. favoriteFoods[2]="Potato"
+// 4. let formerFavoriteFood = favoriteFoods.shift();
+// 5.  favoriteFoods.push("Amala");
+// 6. favoriteFoods.unshift("Roasted Yam");
+ //7. let spliceArray= [2,3,4,5];
+  //  spliceArray.splice(1,1); 
+//    let agb= ["Alpha", "Gamma","Delta"]
+ //      agb.splice(1,0, "Beta");
+ // let numbers= [10,-10,-5,-3,2,1];
+//numbers.splice(1,3,9,8,7,6,5,4,3);
+
+
+
+
+  
