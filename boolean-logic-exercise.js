6f7ced8fc9fc33bc53c7b2bb931c4b638cd4767b
KINGSLEYEHAPA <kessity09@gmail.com>
2021-09-14 06:28:23 +0100

    Boolean logic Exercises Solutions

---
+++ boolean-logic-exercise.js
@@ -0,0 +1,84 @@
+// Part 1-Answers
+// 1. 2== "2"; returns true
+// 2. 2=== 2 ; returns true
+// 3. 10 %3; returns 1
+// 4. 10%3===1 ;  returns true;
+// 5 true&&false; returns false
+// 6.false||true; returns true
+// 8. true||false;  returns true 
+
+
+
+
+// Part 2- Answers
+// 1. let isLearning = true;
+// if(isLearning){
+//    console.log("Keep it up!");
+// } else {
+//     console.log("Pretty sure you are learning....");}
+
+// The above code should display Keep it up! as the condition in the if statement is true.
+
+
+
+
+
+// 2. The IF statement always evaluate if what is in the (), its condition is true before executing its block
+// so no need to write that extra line of code 
+
+
+// let firstvariable;
+// let secondvariable = "";
+// let thirdvariable = 1;
+// let secretMessage = "Shh!";
+
+// if(firstvariable){
+//     console.log("first");
+// } else if(firstvariable || secondvariable){
+//     console.log("second");
+// } else if(firstvariable || thirdvariable){
+//     console.log("third");
+// } else {
+//     console.log("fourth");
+// }
+
+// 1. The console.log output will be Third bacause in the second 'else' statement , thirdvarible is a truthy value.
+// 2. firstvariable is undefined as we did not assign any value to it when we declared it.
+// 3. The value of firstvariable is a falsey value as undefined is false in boolean logic.
+// 4. The value of secondvariable is falsey, empty string is falsey.
+// 5. The value of the thirdvariable is truthy, 1 has a truth value. 
+
+
+// Part 3- Answer:
+
+//1.
+let randomNumber= Math.random();
+console.log(randomNumber);
+if(randomNumber>0.5){
+
+console.log("Over 0.5");
+
+}
+else {
+
+console.log("Under 0.5");
+
+};
+
+// 2.
+// Falsey value are values that are false. The falsey values in javascript are:
+// i. O
+// ii. undefined
+// iii. false 
+// iv. null
+// v.  ""
+// vi. NaN
+
+
+
+
+
+
+
+
+
